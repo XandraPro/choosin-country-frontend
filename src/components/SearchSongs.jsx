67ecc79b9da706c2142c85faf76866e9cfefcce5
SandraPro <xandrapazospro@gmail.com>
2026-03-15 17:01:42 +0100

    SongCard fixed

--- src/components/SearchSongs.jsx
+++ src/components/SearchSongs.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { searchSongs } from "../services/itunes.service";
+import { searchItunes } from "../services/itunes.service";
 import SongCard from "./SongCard";
 
 function SearchSongs() {
@@ -7,27 +7,40 @@ function SearchSongs() {
     const [query, setQuery] = useState("");
     const [songs, setSongs] = useState([]);
 
-    const handleSearch = async (e) => {
-        const results = await searchSongs(query);
-        setSongs(results);
+    const handleSearch = async () => {
+        if (!query) return;
+
+        try {
+            const results = await searchItunes(query);
+            setSongs(results);
+        } catch (error) {
+            console.error('Search error', error)
+        }
     };
 
     return (
         <div>
             <h2>Search Songs</h2>
             <input
+                type='text'
                 value={query}
-                onChange={(e) => setQuery(e.target.value)} 
+                onChange={(e) => setQuery(e.target.value)}
                 placeholder="Search artist or song"
             />
 
             <button onClick={handleSearch}>Search</button>
 
             <div>
-                {songs.map((song) => (
+                {songs.length === 0 ? (
+                    <p>No songs found</p>
+                ): (
+                    songs.map ((song) => (
                     <SongCard key={song.trackId} song={song} />
-                ))}
+                ))
+                )}
             </div>
         </div>
     );
 }
+
+export default SearchSongs;
