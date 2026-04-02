d76a1ccf5d5be2b5fb34d7971fc731fbbd59fc90
SandraPro <xandrapazospro@gmail.com>
2026-04-02 10:18:28 +0200

    fix: own comments and dashboard css

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,17 +1,25 @@
-import { useState} from "react";
+import { useState } from "react";
 import { AuthContext } from "./AuthContext";
 import api from "../api/axios";
 
 function AuthProvider({ children }) {
   const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
 
   const login = async (email, password) => {
     try {
       const res = await api.post("/auth/login", { email, password });
+
       const jwt = res.data.token;
+      const loggedUser = res.data.user;
 
       localStorage.setItem("token", jwt);
+      localStorage.setItem("user", JSON.stringify(loggedUser));
+
       setToken(jwt);
+      setUser(loggedUser);
     } catch (error) {
       console.error("Login error:", error.response?.data || error);
       throw error;
@@ -20,11 +28,13 @@ function AuthProvider({ children }) {
 
   const logout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("user");
     setToken(null);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ token, login, logout }}>
+    <AuthContext.Provider value={{ token, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
